828b2b4f6b8e98189697811dc72f84a67ce24a64
RodrigoMalaquias <48831753+RodrigoMalaquias@users.noreply.github.com>
2022-08-31 21:19:29 -0300

    Adicionando método de "GetAll" de teste dos usuários

---
+++ CodeBase.Test/Users/GetAll/GetAllUsersUseCaseTest.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using CodeBase.Borders.Models;
+using CodeBase.Borders.ViewModel;
+using CodeBase.Repositories.Users;
+using CodeBase.Test.Builders;
+using CodeBase.UseCases.Users.GetAll;
+using Moq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeBase.Test.Users.GetAll
+{
+    public class GetAllUsersUseCaseTest
+    {
+        private readonly GetAllUsersUseCase _getAllUsersUseCase;
+        private readonly Mock<IUserRepository> _userRepository = new();
+        private readonly Mock<IMapper> _mapper = new();
+
+        public GetAllUsersUseCaseTest()
+        {
+            _getAllUsersUseCase = new GetAllUsersUseCase(_userRepository.Object, _mapper.Object);
+        }
+
+        [Fact]
+        public async Task GetAllUsersAsync_WhenUsersAreFound_ReturnUsersAsync()
+        {
+            IEnumerable<User> products = new List<User>
+            {
+                new UserBuilder().AddName("Alisson").AddAge(23).Build(),
+                new UserBuilder().AddName("Diego").AddAge(26).Build()
+            };
+
+            _userRepository.Setup(x => x.GetAllAsync(It.IsAny<CancellationToken>())).ReturnsAsync(products);
+            _mapper.Setup(x => x.Map<IEnumerable<UserViewModel>>(products))
+            .Returns(new List<UserViewModel>
+            {
+                new UserViewModel { Name = "Alisson", Age = 23 },
+                new UserViewModel { Name = "Diego", Age = 26 }
+            });
+
+            var productUseCaseResponse = await _getAllUsersUseCase.Execute(true);
+
+            Assert.NotNull(productUseCaseResponse);
+            Assert.IsType<List<UserViewModel>>(productUseCaseResponse.Result);
+        }
+    }
+
+}
--- CodeBase.UseCases/Users/GetAll/GetAllUsersUseCase.cs
+++ CodeBase.UseCases/Users/GetAll/GetAllUsersUseCase.cs
@@ -10,7 +10,7 @@
     using System.Collections.Generic;
     using System.Threading.Tasks;
 
-    internal class GetAllUsersUseCase : UseCaseBase<bool, IEnumerable<UserViewModel>>, IGetAllUsersUseCase
+    public class GetAllUsersUseCase : UseCaseBase<bool, IEnumerable<UserViewModel>>, IGetAllUsersUseCase
     {
         public override string DefaultErrorMessage => "";
         public override string DefaultSuccessMessage => "";
